aafae0976593e933efc643068fd61d03a93dae12
yaaasmine <yasmine.beqqali00@gmail.com>
2023-11-03 18:13:03 +0100

    test

--- lib/widgets/tasks.dart
+++ lib/widgets/tasks.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:todolist_app/models/task.dart';
 import 'package:todolist_app/widgets/ThemeProvider.dart';
 import 'package:todolist_app/widgets/new_task.dart';
